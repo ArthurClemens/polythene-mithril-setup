e8cf636a26c65cb847f4d156495ab3c559e0ca3e
Arthur Clemens <arthurclemens@gmail.com>
2017-10-31 22:12:47 +0100

    Remove bundle analyzer

--- scripts/webpack.config.prod.js
+++ scripts/webpack.config.prod.js
@@ -2,7 +2,6 @@
 const config = require("./webpack.config.js");
 const webpack = require("webpack");
 const BabiliPlugin = require("babili-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CompressionPlugin = require("compression-webpack-plugin");
 
 config.plugins.push(new webpack.DefinePlugin({
@@ -10,7 +9,6 @@ config.plugins.push(new webpack.DefinePlugin({
 }));
 
 config.plugins.push(new BabiliPlugin());
-config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "static" }));
 config.plugins.push(new CompressionPlugin({
   asset: "[path].gz[query]",
   algorithm: "gzip",
